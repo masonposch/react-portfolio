2c0b5931b00c8e09e16fbf75ee95a24a8a57d7cd
Mason Posch <masonposch@gmail.com>
2017-12-19 00:58:53 -0600

    work examples update

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -9,9 +9,9 @@ class Footer extends React.Component {
 
 		this.state = {
 			social: [
-				{ platform: "Github", logo: "/assets/images/github-logo.png", url: "https://github.com/masonposch"},
-				{ platform: "LinkedIn", logo: "/assets/images/linkedin-logo.png", url: "https://www.linkedin.com/in/mason-posch-7486b599/" },
-				{ platform: "Bitbucket", logo: "/assets/images/bitbucket-logo.png", url: "https://bitbucket.org/masonposch/" },
+				{ platform: "Github", logo: "fa fa-github", url: "https://github.com/masonposch"},
+				{ platform: "Bitbucket", logo: "fa fa-bitbucket", url: "https://bitbucket.org/masonposch/" },
+				{ platform: "LinkedIn", logo: "fa fa-linkedin-square", url: "https://www.linkedin.com/in/mason-posch-7486b599/" },
 			]
 		}
 	}
@@ -21,7 +21,7 @@ class Footer extends React.Component {
 		var socialLinks = this.state.social.map(function(d){
 			return(
 				<a key={d.platform} href={d.url} target="_blank">
-					<img src={d.logo} alt={d.platform} title={d.platform} />
+					<i className={d.logo} aria-hidden="true"></i>
 				</a>
 			);
 		}.bind(this));
@@ -30,10 +30,6 @@ class Footer extends React.Component {
 
 			<div className="footer">
 				<div className="contactSocial">
-					<div className="contactInfo">
-						<p><b>Austin, TX</b></p>
-						<a href="mailto:masonposch@gmail.com">Contact by email</a>
-					</div>
 					<div className="social">
 						{socialLinks}
 					</div>
--- app/components/homepageChildren/WorkSamples.js
+++ app/components/homepageChildren/WorkSamples.js
@@ -12,10 +12,38 @@ class WorkSamples extends React.Component {
 		//Create the state of each project, along with all attributes for the given project
 		this.state = {
 			projects: [
-				{ title: "Bake A Wish", image: "/assets/images/bake-a-wish-logo.png", alt: "Bake A Wish", url: "http://bakeawish.wpengine.com/", target: "_blank" },
-				{ title: "Homeside", image: "/assets/images/homeside-logo.png", alt: "Homeside", url: "https://mmsk-homeside.herokuapp.com/#/login", target: "_blank" },
-				{ title: "Votery", image: "/assets/images/votery-logo.png", alt: "Votery", url: "https://morning-garden-54466.herokuapp.com/votery", target: "_blank" },
-				{ title: "Buyak", image: "/assets/images/buyak-logo.png", alt: "Buyak", url: "https://quiet-peak-65065.herokuapp.com/", target: "_blank" }
+				{ 
+					title: "Bake A Wish", 
+					image: "/assets/images/bake-a-wish-logo.png", 
+					alt: "Bake A Wish", url: "http://bakeawish.wpengine.com/", 
+					target: "_blank",
+					role: "Front-end development, Git version control",
+					description: "A mobile-friendly, passion project by the front-end development team at Kasasa. Local Austin bakery reached out to us to create a custom Wordpress website for them.",
+				},
+				{ 
+					title: "Homeside", 
+					image: "/assets/images/homeside-logo.png", 
+					alt: "Homeside", url: "https://mmsk-homeside.herokuapp.com/#/login", 
+					target: "_blank",
+					role: "Sample role",
+					description: "Sample description", 
+				},
+				{ 
+					title: "Votery", 
+					image: "/assets/images/votery-logo.png", 
+					alt: "Votery", url: "https://morning-garden-54466.herokuapp.com/votery", 
+					target: "_blank",
+					role: "Sample role",
+					description: "Sample description",
+				},
+				{ 
+					title: "Buyak", 
+					image: "/assets/images/buyak-logo.png", 
+					alt: "Buyak", url: "https://quiet-peak-65065.herokuapp.com/", 
+					target: "_blank",
+					role: "Sample role",
+					description: "Sample description", 
+				}
 			]
 		}
 	}
@@ -27,6 +55,10 @@ class WorkSamples extends React.Component {
 				//Individual key for each project being mapped
 				<a key={d.title} href={d.url} target={d.target} className="project">
 						<img src={d.image} title={d.title} alt={d.alt} />
+						<div className="summary">
+							<p className="role"><strong>Role:</strong> {d.role}</p>
+							<p className="description">{d.description}</p>
+						</div>
 				</a>
 			);
 		}.bind(this));
@@ -45,19 +77,3 @@ class WorkSamples extends React.Component {
 
 export default WorkSamples;
 
-
-
-// var checks = this.state.data.map(function(d) {
-//     return (
-//         <div key={d.id} className="checkbox col-lg-3 col-sm-6">
-//             <input type="checkbox" data-id={d.id} checked={d.selected} onChange={this.changeSelection.bind(this, d.id)}/>
-//             {d.id}
-//             <br />
-//         </div>
-//     );
-// }.bind(this));
-// return (
-// 	<div>
-//         {checks}
-//     </div>
-// );
